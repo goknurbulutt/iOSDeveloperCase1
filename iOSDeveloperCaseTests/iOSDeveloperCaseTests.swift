2388be17e1f285364ea8a920b53f74e8129d51ea
goknurbulutt <120587181+goknurbulutt@users.noreply.github.com>
2025-03-04 16:45:59 +0300

    Initial Commit

---
+++ iOSDeveloperCaseTests/iOSDeveloperCaseTests.swift
@@ -0,0 +1,17 @@
+//
+//  iOSDeveloperCaseTests.swift
+//  iOSDeveloperCaseTests
+//
+//  Created by Göknur Bulut on 4.03.2025.
+//
+
+import Testing
+@testable import iOSDeveloperCase
+
+struct iOSDeveloperCaseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
